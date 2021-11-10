54fcf86ce0e846d21319434f32cfbd9f959c2391
Filoteea <s1819162@ed.ac.uk>
2021-11-10 00:08:08 +0000

    filo added baseline script

---
+++ scripts/baseline.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  9 20:25:26 2021
+
+@author: filot
+"""
+# baseline code for smoothing
+# Filoteea Moldovan, University of Edinburgh
+# 10/11/21
+
+
